2ca1974eaa07e4ec1ef7a1d04fbd154da4550333
sbasshub <sbasspro23@gmail.com>
2020-10-30 17:32:08 +0700

    adding some animation but still not perfect yet

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav 
-}
+    const navigation = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+    //toggle nav
+    burger.addEventListener('click', () => {
+        navigation.classList.toggle('.nav-active');
+        // console.log('ok');
+
+        //animate list
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`
+            }
+            // console.log(index / 7);
+        });
+        //🍔animation
+        burger.classList.toggle('.toggle')
+    })
+    
+    
+};
+
+
+navSlide();
